a74e5c126f9384b9d7e535c67cefb7bc46965540
rnemaoh <102898194+nemeleon@users.noreply.github.com>
2022-05-07 21:20:38 +0300

    портфоліо

---
+++ foot.php
@@ -0,0 +1,14 @@
+<footer style="display: flex; flex-direction: row; background-color: black;">
+	<div class="foot-flex">
+		<h3>Pages</h3>
+		<a href="#one">one</a>
+		<a href="#two">two</a>
+	</div>
+	<hr>
+	<div class="foot-flex">
+		<h3>Contacts</h3>
+		<a href="#one">one</a>
+		<a href="#two">two</a>
+	</div>
+</footer>
+</body></html>
---
+++ head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
+    <title><?php echo $title; ?></title>
+    <script type="text/javascript" src="libs/jquerylib.js"></script>
+    <script type="text/javascript" src="scripts/jquerycode.js"></script>
+</head>
+<body>
+<div class="hoverbox"><div class="loader"></div></div>
